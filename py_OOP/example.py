7da92d2cc9cc333d3ba6c08a7b349ad32da150fb
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-24 01:58:13 +0900

    [UPDATE] method

--- py_OOP/example.py
+++ py_OOP/example.py
@@ -1,3 +1,4 @@
+# 1. attributes
 class Sample():
     planet = "Earth" # Class object attribute
 
@@ -10,3 +11,22 @@ stu2 = Sample(name="Mimi", gpa=3.5)
 
 print(stu1.gpa)
 print(stu1.planet)
+
+# 2. Method
+print()
+class Circle():
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return Circle.pi * self.radius ** 2 #self.pi
+    
+    def perimeter(self):
+        return self.pi * 2 * self.radius
+
+myCircle = Circle(3)
+print(myCircle.radius)
+print(myCircle.area())
+print(myCircle.perimeter())
