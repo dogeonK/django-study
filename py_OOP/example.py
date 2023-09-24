a77d1f71a1229101f1088ab954f6928b10ef7082
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-25 00:07:57 +0900

    [UPDATE] 상속

--- py_OOP/example.py
+++ py_OOP/example.py
@@ -30,3 +30,38 @@ myCircle = Circle(3)
 print(myCircle.radius)
 print(myCircle.area())
 print(myCircle.perimeter())
+
+# 3. Inheritance
+print()
+class Person():
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def hello(self):
+        print("hello")
+    
+    def report(self):
+        print(f"I am {self.first_name} {self.last_name}")
+
+
+class Agent(Person):
+
+    def report(self):
+        print("I am here")
+
+    def reveal(self, passcode):
+        if (passcode == 123):
+            print("I am secret agent")
+        else:
+            self.report()
+
+x = Person("John", "Smith")
+x.hello()
+x.report()
+
+y = Agent("John", "Smith")
+y.hello()
+y.report()
+y.reveal(123)
