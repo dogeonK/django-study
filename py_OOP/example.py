ab7694ae732e1d46928333c9a5aed22ab8c0b2f2
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-26 01:53:47 +0900

    [UPDATE] Dunder method

--- py_OOP/example.py
+++ py_OOP/example.py
@@ -65,3 +65,22 @@ y = Agent("John", "Smith")
 y.hello()
 y.report()
 y.reveal(123)
+
+# 4. Special (Dunder) Methods
+print()
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"{self.title} written by {self.author}"
+    
+    def __len__(self):
+        return self.pages
+
+myBook = Book('Python', "Jose", 120)
+print(myBook)
+print(len(myBook))
