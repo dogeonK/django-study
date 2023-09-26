5bc7e71778d207e191d32e066779c6a5142ed19d
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-27 02:07:23 +0900

    [UPDATE] module & package

---
+++ py_OOP/mymodule.py
@@ -0,0 +1,10 @@
+def useful_func():
+    print("Using the userful_func!")
+
+class UsefulClass():
+
+    def __init__(self, message):
+        self.message = message
+    
+    def report(self):
+        print(self.message)
---
+++ py_OOP/myprogram.py
@@ -0,0 +1,9 @@
+from mymodule import *
+from mypackage.mysubmodule import my_sub_func
+
+useful_func()
+
+myInstance = UsefulClass("hello")
+myInstance.report()
+
+my_sub_func()
