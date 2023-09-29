cc8f283ab03c931562f1b9d9653f930229b6c9db
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-30 00:18:15 +0900

    [UPDATE] 동적 뷰 라우팅

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+articles = {
+    'sports' : 'Sports Page',
+    'finance' : 'Finance Page',
+    'politics' : 'Politics Page',
+}
+
 # Create your views here.
 def simple_view(request):
     return HttpResponse("Simple View")
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
