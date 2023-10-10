b5e91d629f6ee8ddf5df10e9f1f6af9985d7fa67
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-10-10 22:00:17 +0900

    [UPDATE] 템플릿에 context 전달

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def example_view(request):
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+    
+    my_var = {'first_name' : 'Rosalind', 'last_name' : 'Franklin'
+            , 'some_list' : [1, 2, 3], 'some_dict' : {'inside_key' : 'inside_value'}
+    }
+    
+    return render(request, 'my_app/variable.html', context=my_var)
