509d90eb05ae6982d6c7697432ea3195be7e71c8
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-10-12 18:37:07 +0900

    [UPDATE] if

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -8,7 +8,8 @@ def example_view(request):
 def variable_view(request):
     
     my_var = {'first_name' : 'rosaLind', 'last_name' : 'franklin'
-            , 'some_list' : [1, 2, 3], 'some_dict' : {'inside_key' : 'inside_value'}
+            , 'some_list' : [1, 2, 3], 'some_dict' : {'inside_key' : 'inside_value'}, 
+            'user_logged_in' : True
     }
     
     return render(request, 'my_app/variable.html', context=my_var)
