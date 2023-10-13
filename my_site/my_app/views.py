8ce5ffeb3f6b7c4cf7b864192a3ccb8b752c3d6e
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-10-13 22:27:48 +0900

    [UPDATE] 템플릿 태그, url name

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -7,9 +7,5 @@ def example_view(request):
 
 def variable_view(request):
     
-    my_var = {'first_name' : 'rosaLind', 'last_name' : 'franklin'
-            , 'some_list' : [1, 2, 3], 'some_dict' : {'inside_key' : 'inside_value'}, 
-            'user_logged_in' : True
-    }
     
-    return render(request, 'my_app/variable.html', context=my_var)
+    return render(request, 'my_app/variable.html')
